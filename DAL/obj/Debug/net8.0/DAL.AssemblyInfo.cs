a4282dcc160f64ef82b4cc1b58e40f68fb78f0da
Huykocony35 <huylovengoc35@gmail.com>
2025-01-04 15:36:00 +0700

    BLL files structure

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c676db5f5dca028388d880dda7fc511485350450")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3555ff878dc30d29fb5a36c562fe7094112b921c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
